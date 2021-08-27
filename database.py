52bd691f4360ad2ca10d6e974c26c8377663d010
Kvizas <theqxizgaming@gmail.com>
2021-08-27 12:41:44 +0300

    DB Change

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://auth-be@34.116.185.239/SS-Auth"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://sql11433143@sql11.freesqldatabase.com/sql11433143"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
